6f5e5d7ea8d89eba9dd684600398ac7c55351639
Walter Alberto COARASA <coarasaw@gmail.com>
2023-06-15 18:16:40 -0300

    Add files via upload

--- src/app/paciente/paciente-routing.module.ts
+++ src/app/paciente/paciente-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PacienteGuard } from '../guards/paciente.guard';
+import { CancelarTurnoComponent } from './cancelar-turno/cancelar-turno.component';
+import { MisTurnosComponent } from './mis-turnos/mis-turnos.component';
+import { PerfilPacienteComponent } from './perfil-paciente/perfil-paciente.component';
+import { SolicitarTurnoComponent } from './solicitar-turno/solicitar-turno.component';
 
 const routes: Routes = [
-
+  { path: 'misTurnos', component: MisTurnosComponent,canActivate: [PacienteGuard]},
+  { path: 'solicitarTurno', component: SolicitarTurnoComponent,canActivate: [PacienteGuard]},
+  { path: 'cancelarTurno', component: CancelarTurnoComponent,canActivate: [PacienteGuard]},
+  { path: 'perfilPaciente', component: PerfilPacienteComponent,canActivate: [PacienteGuard]},
   { path: '', redirectTo: 'bienvenidoLogin', pathMatch: 'full' },
   { path: '**', pathMatch: 'full', redirectTo: 'bienvenidoLogin' }
 ];
--- src/app/paciente/paciente.module.ts
+++ src/app/paciente/paciente.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PacienteRoutingModule } from './paciente-routing.module';
+import { MisTurnosComponent } from './mis-turnos/mis-turnos.component';
+import { SolicitarTurnoComponent } from './solicitar-turno/solicitar-turno.component';
+import { CancelarTurnoComponent } from './cancelar-turno/cancelar-turno.component';
+import { PerfilPacienteComponent } from './perfil-paciente/perfil-paciente.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MisTurnosComponent,
+    SolicitarTurnoComponent,
+    CancelarTurnoComponent,
+    PerfilPacienteComponent
+  ],
   imports: [
     CommonModule,
-    PacienteRoutingModule
+    PacienteRoutingModule,
+    SharedModule
   ]
 })
 export class PacienteModule { }
