17d8099ab7697a13d49592a89394036526877eab
Walter Alberto COARASA <coarasaw@gmail.com>
2023-06-22 17:41:43 -0300

    Add files via upload

---
+++ src/app/interfaces/perfil.ts
@@ -0,0 +1,12 @@
+export interface Perfil {
+  perfil: string;
+  apellido: string;
+  nombre: string;
+  dni:string;
+  email: string;
+  aprobadoPorAdmin?:string,
+  especialidad?:string;
+  otraEspecialidad?:string;
+  fotoPerfilUno?:string;
+  fotoPerfilDos?:string;
+}
---
+++ src/app/interfaces/turno.ts
@@ -0,0 +1,11 @@
+export interface Turno {
+  apellido: string;
+  nombre: string;
+  email: string;
+  especialidad?:string;
+  otraespecialidad?:string;
+  turno:string;
+  fotoPerfilUno:string;
+  estado:string;
+  dia:string;
+}
